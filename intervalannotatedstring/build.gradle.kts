1d961466070fe742d012100387d86271ba239386
Alex Dadukin <alexanderdadukin@gmail.com>
2025-08-04 15:14:45 +0100

    Setup maven publishing script

--- intervalannotatedstring/build.gradle.kts
+++ intervalannotatedstring/build.gradle.kts
@@ -1,10 +1,13 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import org.jetbrains.kotlin.gradle.plugin.extraProperties
 
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.detekt)
+    id("maven-publish")
 }
 
 android {
@@ -12,8 +15,16 @@ android {
     compileSdk = 36
 
     defaultConfig {
+        version = extraProperties.get("publish.versionName").toString()
+
         minSdk = 21
         consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
     }
 
     kotlin {
@@ -33,6 +44,61 @@ android {
 
     composeOptions {
         kotlinCompilerExtensionVersion = libs.versions.compose.toString()
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        repositories {
+            maven {
+                name = "local"
+                url = uri(layout.buildDirectory.dir("maven-local"))
+            }
+
+            maven {
+                name = "maven-central"
+
+                val releaseUrl = extraProperties.get("publish.repo.releaseUrl").toString()
+                val snapshotUrl = extraProperties.get("publish.repo.snapshotUrl").toString()
+
+                url = uri(if (BuildConfig.isSnapshot) snapshotUrl else releaseUrl)
+                credentials {
+                    val localProperties = gradleLocalProperties(project.rootDir, providers)
+                    username = localProperties.getProperty("publish.repo.credentials.userName")
+                    password = localProperties.getProperty("publish.repo.credentials.password")
+                }
+            }
+        }
+
+        register<MavenPublication>("release") {
+            groupId = extraProperties.get("publish.groupId").toString()
+            artifactId = extraProperties.get("publish.artifactId").toString()
+            version = extraProperties.get("publish.versionName").toString()
+
+            pom {
+                name = extraProperties.get("publish.pom.name").toString()
+                description = extraProperties.get("publish.pom.description").toString()
+                url = extraProperties.get("publish.pom.url").toString()
+
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+            }
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
     }
 }
 
