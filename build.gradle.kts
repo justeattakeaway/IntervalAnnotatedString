97ce7d31a4441f3163ef468e4084d3b65cb57878
Mez Pahlan <mez.pahlan@just-eat.com>
2025-08-05 11:22:17 +0100

    Add binary compatibility validator
    This commit introduces the Kotlin binary compatibility validator plugin
    to the project.
    
    The plugin is configured to ignore the "app" project for API validation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,9 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.binary.compatibility.validator)
+}
+
+apiValidation {
+    ignoredProjects.addAll(listOf("app"))
 }
